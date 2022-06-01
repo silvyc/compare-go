a0d061f50f308c80e3a0d84b109ffd99e0c59db5
silvyc <smchain@uninorte.edu.co>
2022-06-01 18:01:54 -0500

    Update routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Country from "../views/country";
 import About from "../views/about";
-import Daily from "../views/daily"
 import Home from "../views/home";
 
 const Routing = () => (
