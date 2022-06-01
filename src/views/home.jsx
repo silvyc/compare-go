89ef53990cbb2db4b4d3234c644210d1d83a0efc
silvyc <smchain@uninorte.edu.co>
2022-06-01 17:55:02 -0500

    Update home.jsx

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -13,21 +13,7 @@ export default function Home() {
             What is COVID-19?
           </Text>
           <Text textAlign="justify" mb={3}>
-            Coronavirus disease (COVID-19) is an infectious disease caused by
-            the SARS-CoV-2 virus. Most people infected with the virus will
-            experience mild to moderate respiratory illness and recover without
-            requiring special treatment. However, some will become seriously ill
-            and require medical attention. Older people and those with
-            underlying medical conditions like cardiovascular disease, diabetes,
-            chronic respiratory disease, or cancer are more likely to develop
-            serious illness. Anyone can get sick with COVID-19 and become
-            seriously ill or die at any age. The best way to prevent and slow
-            down transmission is to be well informed about the disease and how
-            the virus spreads. Protect yourself and others from infection by
-            staying at least 1 metre apart from others, wearing a properly
-            fitted mask, and washing your hands or using an alcohol-based rub
-            frequently. Get vaccinated when it’s your turn and follow local
-            guidance.
+            
           </Text>
           <Text textAlign="justify">
             The virus can spread from an infected person’s mouth or nose in
